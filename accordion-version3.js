cb89ec094320f5fd2ceae1a59b214e324c296d7a
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-05-01 18:00:31 -0500

    feat: basic accordion fully functional but all shelves remain open when another is clciked on

--- accordion-version3.js
+++ accordion-version3.js
@@ -1,11 +1,24 @@
 //Accordion Basic Mechanics
 let jsaccordion = {
-    init : function(target){
-    //init() : initialize accordion
-    // PARAM target : ID of accordion wrapper
+    init : function(target) {
+        //init() : initialize accordion
+        // PARAM target : ID of accordion wrapper
 
-    var headers = document.querySelectorAll("#" + target + " .accordion-head");
-    if (headers.length > 0){
-        head.addEventListener("click", jsaccordion.select);
-}}
-}
+        let headers = document.querySelectorAll("#" + target + " .accordion-head");
+        if (headers.length > 0) {
+            for (var head of headers) {
+                head.addEventListener("click", jsaccordion.select);
+            }
+        }
+    },
+    select : function(){
+        let contents = this.nextElementSibling;
+        contents.classList.toggle("open");
+    }
+};
+
+//on page load
+window.addEventListener('load', function(){
+    jsaccordion.init("accordion-basic");
+});
+
